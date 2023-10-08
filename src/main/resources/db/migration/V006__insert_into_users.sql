c5279fc9876865f83766f09a7935503566f492be
eskil <eskiltb@hotmail.com>
2023-10-08 18:29:07 +0200

    More migrations, properties -> yaml, add flyway

--- src/main/resources/db/migration/V006__insert_into_users.sql
+++ src/main/resources/db/migration/V006__insert_into_users.sql
@@ -1,4 +1,4 @@
-INSERT INTO users (username, password, role_id) VALUES ('user', 'password', 1); -- 'user' role
-INSERT INTO users (username, password, role_id) VALUES ('moderator', 'password', 2); -- 'moderator' role
-INSERT INTO users (username, password, role_id) VALUES ('admin', 'password', 3); -- 'admin' role
-INSERT INTO users (username, password, role_id) VALUES ('owner', 'password', 4); -- 'owner' role
+INSERT INTO users (id, username, password, role_id) VALUES ((SELECT nextval('user_id_generator')), 'owner', 'password', 4); -- 'owner' role
+INSERT INTO users (id, username, password, role_id) VALUES ((SELECT nextval('user_id_generator')), 'admin', 'password', 3); -- 'admin' role
+INSERT INTO users (id, username, password, role_id) VALUES ((SELECT nextval('user_id_generator')), 'moderator', 'password', 2); -- 'moderator' role
+INSERT INTO users (id, username, password, role_id) VALUES ((SELECT nextval('user_id_generator')), 'user', 'password', 1); -- 'user' role
