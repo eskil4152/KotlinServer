e123754d6e0db65d988ed0db7e5109fc057b3712
eskil <eskiltb@hotmail.com>
2023-10-08 14:48:18 +0200

    Minor changes to properties, remade controller to work with existing client

--- src/main/kotlin/com/blikeng/kotlinServer/controllers/PeopleController.kt
+++ src/main/kotlin/com/blikeng/kotlinServer/controllers/PeopleController.kt
@@ -6,6 +6,7 @@ import jakarta.websocket.server.PathParam
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -19,18 +20,18 @@ class PeopleController(@Autowired private val peopleService: PeopleService) {
         return ResponseEntity.ok(peopleService.getAllPeople(page).toList())
     }
 
-    @GetMapping("/first")
-    fun findByFirstName(@RequestBody firstName: String): ResponseEntity<List<Person>> {
+    @GetMapping("/search/first/{firstName}")
+    fun findByFirstName(@PathVariable firstName: String): ResponseEntity<List<Person>> {
         return ResponseEntity.ok(peopleService.getPeopleByFirstName(firstName))
     }
 
-    @GetMapping("/last")
-    fun findByLastName(@RequestBody lastName: String): ResponseEntity<List<Person>> {
+    @GetMapping("/search/last/{lastName}")
+    fun findByLastName(@PathVariable lastName: String): ResponseEntity<List<Person>> {
         return ResponseEntity.ok(peopleService.getPeopleByLastName(lastName))
     }
 
-    @GetMapping("/full")
-    fun getByFullName(@RequestBody firstName: String, lastName: String): ResponseEntity<List<Person>> {
+    @GetMapping("/search/full/{firstName}/{lastName}")
+    fun getByFullName(@PathVariable firstName: String, lastName: String): ResponseEntity<List<Person>> {
         return ResponseEntity.ok(peopleService.getPersonByFullName(firstName, lastName))
     }
 }
